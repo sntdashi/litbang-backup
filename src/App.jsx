e29227b01971d14c89bf70305839b29ee0a88a86
sntdashi <sntdashi@gmail.com>
2025-11-12 19:31:27 +0000

    update lagi

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Tentang from "./Pages/Tentang";
 import AnimatedBackground from "./components/Background";
 import Navbar from "./components/Navbar";
 import LitbangWeb from "./Pages/LitbangWeb";
-import ContactPage from "./Pages/Kontak";
+import ContactPage from "./Pages/Contact";
 import ProjectDetails from "./components/ProjectDetail";
 import WelcomeScreen from "./Pages/WelcomeScreen";
 import { AnimatePresence } from 'framer-motion';
