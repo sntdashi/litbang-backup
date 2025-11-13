b544bb601144d838b6dfe93be62bc4b69d0f0573
sntdashi <sntdashi@gmail.com>
2025-11-13 02:41:50 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
           <AnimatedBackground />
           <Home />
           <Tentang />
-          <Litbang Web />
+          <LitbangWeb />
           <ContactPage />
           <footer>
             <center>
@@ -36,7 +36,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2025{" "}
                 <a href="https://flowbite.com/" className="hover:underline">
-                  rawrzn
+                  himatif
                 </a>
                 . All Rights Reserved.
               </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const navItems = [
         { href: "#Home", label: "Home" },
         { href: "#Tentang", label: "Tentang" },
-        { href: "#Litbang Web", label: "Litbang Web" },
+        { href: "#LitbangWeb", label: "Litbang Web" },
         { href: "#Kontak", label: "Kontak" },
     ];
 
@@ -84,7 +84,7 @@ const Navbar = () => {
                             onClick={(e) => scrollToSection(e, "#Home")}
                             className="text-xl font-bold bg-gradient-to-r from-[#a855f7] to-[#6366f1] bg-clip-text text-transparent"
                         >
-                            rawrzn
+                            litbang
                         </a>
                     </div>
         
